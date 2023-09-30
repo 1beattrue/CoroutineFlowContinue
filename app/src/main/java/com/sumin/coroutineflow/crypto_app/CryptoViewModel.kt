9a8cefa3c0e529d8fb850f00ffdee3e3df73ac32
Vitaly <onebeattrue@gmail.com>
2023-09-30 04:01:59 +0300

    asLiveData() способна отслеживать смену конфигурации и прекращать загрузку в фоне например Также у него есть дефолтный таймаут около 5 сек перед отменой корутины

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -1,41 +1,31 @@
 package com.sumin.coroutineflow.crypto_app
 
+import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
+import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.launch
 
 class CryptoViewModel : ViewModel() {
 
+
     private val repository = CryptoRepository
 
-    private val _state = MutableLiveData<State>(State.Initial)
-    val state: LiveData<State> = _state
-
-    init {
-        loadData()
-    }
-
-    private fun loadData() {
-        repository.getCurrencyList()
-            .onStart {
-                val currentState = _state.value
-                if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
-                    _state.value = State.Loading
-                }
-            }
-            .filter { it.isNotEmpty() }
-            .onEach {
-                _state.value = State.Content(currencyList = it)
-            }
-            .launchIn(viewModelScope) // под капотом viewModelScope.launch { collect() }
-    }
+    val state: LiveData<State> = repository.getCurrencyList()
+        .filter { it.isNotEmpty() }
+        .map { State.Content(currencyList = it) as State }
+        .onStart {
+            emit(State.Loading)
+            Log.d("CryptoViewModel", "Started")
+        }
+        .onEach {
+            Log.d("CryptoViewModel", "onEach")
+        }
+        .onCompletion {
+            Log.d("CryptoViewModel", "Complete")
+        }.asLiveData()
 }
