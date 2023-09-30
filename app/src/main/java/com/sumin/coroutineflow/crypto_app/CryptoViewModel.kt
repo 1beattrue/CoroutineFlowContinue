daea7d26523879b439406144b06ba192972409a0
Vitaly <onebeattrue@gmail.com>
2023-09-30 03:14:52 +0300

    Под капотом onStart {} находится FlowBuilder, поэтому из него можно emit'ить значения Объект Flow можно вернуть в LiveData, используя метод asLiveData()

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -3,11 +3,13 @@ package com.sumin.coroutineflow.crypto_app
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
@@ -17,25 +19,11 @@ class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    private val _state = MutableLiveData<State>(State.Initial)
-    val state: LiveData<State> = _state
-
-    init {
-        loadData()
-    }
-
-    private fun loadData() {
-        repository.getCurrencyList()
-            .onStart {
-                val currentState = _state.value
-                if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
-                    _state.value = State.Loading
-                }
-            }
-            .filter { it.isNotEmpty() }
-            .onEach {
-                _state.value = State.Content(currencyList = it)
-            }
-            .launchIn(viewModelScope) // под капотом viewModelScope.launch { collect() }
-    }
+    val state: LiveData<State> = repository.getCurrencyList()
+        .filter { it.isNotEmpty() }
+        .map { State.Content(currencyList = it) as State }
+        .onStart {// вызывается только при подписке на объект
+            emit(State.Loading)
+        }
+        .asLiveData()
 }
