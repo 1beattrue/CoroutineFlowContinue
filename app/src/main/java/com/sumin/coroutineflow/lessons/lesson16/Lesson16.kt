b612da59b5b6601f90520b21961af9fab5b4bff2
Vitaly <onebeattrue@gmail.com>
2023-10-01 22:28:52 +0300

    Exception Handling Правильный способ реализации учитывая "принцип прозрачности исключений"
    метод catch {} ловит ошибки, которые произошли выше него

--- app/src/main/java/com/sumin/coroutineflow/lessons/lesson16/Lesson16.kt
+++ app/src/main/java/com/sumin/coroutineflow/lessons/lesson16/Lesson16.kt
@@ -2,23 +2,39 @@ package com.sumin.coroutineflow.lessons.lesson16
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 suspend fun main() {
-    val flow = loadDataFlow()
-
-    flow.collect {
-        throw RuntimeException("Exception from collect")
-    }
+    loadDataFlow()
+        .map { State.Content(it) as State }
+        .onStart {
+            emit(State.Loading)
+        }
+        .catch {
+            emit(State.Error)
+        }
+        .collect {
+            when (it) {
+                is State.Content -> println(it.value)
+                State.Error -> println("something went wrong")
+                State.Loading -> println("loading...")
+            }
+        }
 }
 
 fun loadDataFlow(): Flow<Int> = flow {
-    try {
-        repeat(5) {
-            delay(500)
-            emit(it)
-        }
-    } catch (e: Exception) {
-        println(e) // ловит исключение из блока collect, потому что на низком уровне collect работает именно с emit
+    repeat(5) {
+        delay(500)
+        emit(it)
     }
+    throw RuntimeException("Exception from flow block")
+}
+
+sealed class State {
+    data class Content(val value: Int) : State()
+    data object Loading : State()
+    data object Error : State()
 }
